2525cec4bae2c2079bc032ff22d7ffa0b9fd909e
Hossien7 <139698226+Hossien7@users.noreply.github.com>
2025-07-28 19:22:13 +0330

    UPDATE: if condition added

---
+++ 3-if condition.py
@@ -0,0 +1,11 @@
+rank = int(input("shoma chandom shodi? "))
+
+if rank == 1:
+    print("gold")
+elif rank == 2:
+    print("bronz")
+elif rank == 3:
+    print("no medal")
+else:
+    print("no medal")
+    
