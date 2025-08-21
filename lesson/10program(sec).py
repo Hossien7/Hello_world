24e073c0817306f11f959c37b48a45661f7b6fd0
Hossien7 <139698226+Hossien7@users.noreply.github.com>
2025-08-21 14:50:33 +0330

    Done: Secound 10 Program task done

--- lesson/10program(sec).py
+++ lesson/10program(sec).py
@@ -1,5 +1,22 @@
 lst = [1, 4, 32, 5, 4, 234, 5, 1, 5]
 
+def sum_of_even_and_odd(lst):  # 1 Done
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_sum = 0
+    odd_sum = 0
+    index = 0
+
+    while index < len(numbers):
+        if numbers[index] % 2 == 0:
+            even_sum += numbers[index]
+        else:
+            odd_sum += numbers[index]
+        index += 1
+
+    print("Sum of Even Numbers:", even_sum)
+    print("Sum of Odd Numbers:", odd_sum)
+
+
 def smallest_num(lst):  # 2 Done
     lst = [10, 4, 32, 5, 4, 234, 5, 1, 5]
     head = 0
@@ -13,8 +30,53 @@ def smallest_num(lst):  # 2 Done
 print(f'smallest num in your list is: {smallest_num(lst=lst)}')
 
 
-def triangle(num):  # 3
-    pass
+def triangle(num=10):  # 3 Done
+    i = 0
+    while i < num:
+        print("*" * (i + 1))
+        i += 1
+
+
+def reverse_string(string):  # 4 Done
+    string = 'iran'
+    tail = -1
+    while tail >= -len(string):
+        print(string[tail])
+        tail -= 1
+
+def fact(num):  # 5 Done
+    factorial = 1
+    counter = 1
+    while counter <= num:
+        factorial *= counter
+        counter += 1
+
+    print("Factorial of", num, "is", factorial)
+
+def check_prime(num):  # 6 Done
+    is_prime = False
+    if num < 2:
+        return is_prime
+    else:
+        i = 2
+        is_prime = True
+        while i < num:
+            if num % i == 0:
+                is_prime = False
+                break
+            i += 1
+        return is_prime
+
+def fibo(n):  # 7 Done
+    a, b = 0, 1
+    counter = 0
+
+    while counter < n:
+        print(f'{a} ,', end=' ')
+        nth = a + b
+        a = b
+        b = nth
+        counter += 1
 
 
 def number_game():  # 8 Done
@@ -31,3 +93,27 @@ def number_game():  # 8 Done
         elif user_num < target:
             print("your choice is smaller than target")
 
+
+
+
+
+
+
+def numbers(n): # 9 Done
+    number = n
+    multiplier = 1
+    while multiplier <= 10:
+        print(number, "x", multiplier, "=", number * multiplier)
+        multiplier += 1
+numbers(10)
+
+def sum_of_digit(lst):  # 10 Done
+    sum = 0
+    head = 0
+    while head < len(lst):
+        if type(lst[head]) == int:
+            sum += lst[head]
+        head += 1
+    print(sum)
+print(sum_of_digit(lst=lst))
+print(fibo(10))
