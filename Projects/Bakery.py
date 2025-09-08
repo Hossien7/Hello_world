436b12a22df9c266e775c8b715d2adfe6d0793c0
Hossien7 <139698226+Hossien7@users.noreply.github.com>
2025-09-08 14:42:58 +0330

    DONE: Bakery task
    Bakery task done

--- Projects/Bakery.py
+++ Projects/Bakery.py
@@ -2,8 +2,14 @@ bread = [('Barbary', 5000), ('Lavash', 2000), ('Sangak', 15000), ('Taftoon', 500
 sum = 0
 while True:
     print('Welcom to DONDI Bakery!')
-    print(f'1.{bread[0][0]}\n2.{bread[1][0]}\n3.{bread[2][0]}\n4.{bread[3][0]}\n5.{bread[4][0]}\n')
-    user_bread = input('Enter your Bread index: ')
-    if int(user_bread):
-        couser_bread = int(user_bread)
-        sum += bread[(user_bread - 1)]
+    answer_of_user = input('Do You Want Bread? y/n: ')
+    if answer_of_user == 'y':
+        print(f'1.{bread[0][0]}\n2.{bread[1][0]}\n3.{bread[2][0]}\n4.{bread[3][0]}\n5.{bread[4][0]}')
+        user_bread = input('Enter your Bread index: ')
+        if int(user_bread):
+            couser_bread = int(user_bread)
+            sum += bread[(couser_bread - 1)][1]
+    else:
+        break
+
+print(f'Sum of your bread cost: {sum}')
